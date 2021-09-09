7c8cbc850325518bf9be400f329fa6208ddd452d
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-09-09 11:37:54 +0530

    Adding Part time employee and wage

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import random
 
-IS_PRESENT = 1
+IS_PRESENT_FULL_DAY = 1
+IS_PRESENT_PART_TIME = 2
 WAGE_PER_HOUR = 20
 FULL_DAY_HOURS = 8
+PART_TIME_HOURS = 4
 
-check_emp = random.randint(0,1)
-if check_emp == IS_PRESENT:
-    print("Employee is present and the wage is = " +str (WAGE_PER_HOUR * FULL_DAY_HOURS))
+check_emp = random.randint(0,2)
+if check_emp == IS_PRESENT_FULL_DAY:
+    print("Employee is present for full time and the wage is = "+str(WAGE_PER_HOUR * FULL_DAY_HOURS))
+elif check_emp == IS_PRESENT_PART_TIME:
+    print("Employee is present for part time and wage is = "+str (WAGE_PER_HOUR * PART_TIME_HOURS))
 else:
     print("Employee is absent")
