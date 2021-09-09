4baba34709abd78592d94fcfd82a2f59abf14785
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-09-09 11:44:44 +0530

    using dictionary

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import random
 
+IS_ABSENT = 0
 IS_PRESENT_FULL_DAY = 1
 IS_PRESENT_PART_TIME = 2
 WAGE_PER_HOUR = 20
 FULL_DAY_HOURS = 8
 PART_TIME_HOURS = 4
 
+emp_attendence = {
+    IS_PRESENT_FULL_DAY :
+    "Employee is present for full time and the wage is = "+str(WAGE_PER_HOUR * FULL_DAY_HOURS),
+    IS_PRESENT_PART_TIME:
+    "Employee is present for part time and wage is = "+str (WAGE_PER_HOUR * PART_TIME_HOURS),
+    IS_ABSENT:
+    "Employee is absent"
+    }
+
 check_emp = random.randint(0,2)
-if check_emp == IS_PRESENT_FULL_DAY:
-    print("Employee is present for full time and the wage is = "+str(WAGE_PER_HOUR * FULL_DAY_HOURS))
-elif check_emp == IS_PRESENT_PART_TIME:
-    print("Employee is present for part time and wage is = "+str (WAGE_PER_HOUR * PART_TIME_HOURS))
-else:
-    print("Employee is absent")
+print(emp_attendence.get(check_emp))
