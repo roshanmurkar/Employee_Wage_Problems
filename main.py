22f3ccc4eca21581f2d1e6f468b1361443f9eb58
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-09-09 12:02:24 +0530

    total wage with condition

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ WAGE_PER_HOUR = 20
 FULL_DAY_HOURS = 8
 PART_TIME_HOURS = 4
 TOTAL_WORKING_DAYS = 20
+TOTAL_WORKING_HOURS = 100
 
 emp_attendence = {
-    IS_PRESENT_FULL_DAY : WAGE_PER_HOUR * FULL_DAY_HOURS,
-    IS_PRESENT_PART_TIME: WAGE_PER_HOUR * PART_TIME_HOURS,
+    IS_PRESENT_FULL_DAY: FULL_DAY_HOURS,
+    IS_PRESENT_PART_TIME: PART_TIME_HOURS,
     IS_ABSENT: 0
-    }
+}
 
-totalWage = 0
-check_emp = random.randint(0,2)
-for i in range(TOTAL_WORKING_DAYS):
-    totalWage = totalWage + emp_attendence.get(check_emp)
+workingDays = 0
+totalWorkingHours = 0
 
-print("Employee total wage is ",totalWage)
+check_emp = random.randint(0, 2)
+while (workingDays < TOTAL_WORKING_DAYS and totalWorkingHours < TOTAL_WORKING_HOURS):
+    totalWorkingHours = totalWorkingHours + emp_attendence.get(check_emp)
+    workingDays = workingDays + 1
+
+totalWage = totalWorkingHours * WAGE_PER_HOUR
+print("Employee total wage is ", totalWage)
