9919c5c1abcd004953683c36fb7db305dedc9c07
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-09-09 12:11:34 +0530

    using function

--- main.py
+++ main.py
@@ -8,20 +8,23 @@ FULL_DAY_HOURS = 8
 PART_TIME_HOURS = 4
 TOTAL_WORKING_DAYS = 20
 TOTAL_WORKING_HOURS = 100
+global workingDays
 
-emp_attendence = {
-    IS_PRESENT_FULL_DAY: FULL_DAY_HOURS,
-    IS_PRESENT_PART_TIME: PART_TIME_HOURS,
-    IS_ABSENT: 0
-}
+def calculateEmployeeWage():
 
-workingDays = 0
-totalWorkingHours = 0
+    emp_attendence = {
+        IS_PRESENT_FULL_DAY : FULL_DAY_HOURS,
+        IS_PRESENT_PART_TIME: PART_TIME_HOURS,
+        IS_ABSENT: 0
+        }
+    workingDays = 0
+    totalWorkingHours = 0
+    check_emp = random.randint(0, 2)
+    while(workingDays < TOTAL_WORKING_DAYS and totalWorkingHours < TOTAL_WORKING_HOURS):
+        totalWorkingHours = totalWorkingHours + emp_attendence.get(check_emp)
+        workingDays = workingDays + 1
 
-check_emp = random.randint(0, 2)
-while (workingDays < TOTAL_WORKING_DAYS and totalWorkingHours < TOTAL_WORKING_HOURS):
-    totalWorkingHours = totalWorkingHours + emp_attendence.get(check_emp)
-    workingDays = workingDays + 1
+    return totalWorkingHours * WAGE_PER_HOUR
 
-totalWage = totalWorkingHours * WAGE_PER_HOUR
-print("Employee total wage is ", totalWage)
+totalWage = calculateEmployeeWage()
+print("Employee total wage is ",totalWage)
